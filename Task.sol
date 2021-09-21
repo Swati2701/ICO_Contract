73b75e80c2f302110bcc1abcaf46fddfde385f2e
Swati Jindal <88382946+Swati2701@users.noreply.github.com>
2021-09-21 12:50:44 +0530

    Add files via upload

--- Task.sol
+++ Task.sol
@@ -5,11 +5,13 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./MyToken.sol";
 import "./CrowdSale.sol";
 
-contract task is Ownable{
+contract Task is Ownable{
     
     uint256 totalSupply;
     uint256 saleQuantity;
     address payable addr;
+    uint256 tokensValue; //Convert ether into token 
+       
     enum ICOStage { PreSale, secondSale, remainingSale}
   
     ICOStage public stage = ICOStage.PreSale;
@@ -18,7 +20,8 @@ contract task is Ownable{
           totalSupply = _totalSupply;
       }
 
-    
+   
+
     function setICOStage(uint _stage) public onlyOwner{
      if(uint(ICOStage.PreSale) == _stage) {
          stage = ICOStage.PreSale;
@@ -27,32 +30,47 @@ contract task is Ownable{
             } else if(uint(ICOStage.remainingSale) == _stage){
                 stage = ICOStage.remainingSale;
             }
-     if(stage == ICOStage.PreSale) { 
-         saleQuantity = 30000000;
+    }
+    function buy() public payable{
+         if(stage == ICOStage.PreSale) { 
+         saleQuantity = 30000000; 
+         while(saleQuantity!=0){
+             tokensValue = (msg.value) * 1e5 / 1 ether;
+             saleQuantity -= tokensValue;
+             addr.transfer(saleQuantity);
+         }
        } else if (stage == ICOStage.secondSale) {
-           saleQuantity = 50000000;
+           
+           saleQuantity = 50000000; 
+           while(saleQuantity!=0){
+             tokensValue = (msg.value) * 1e5 / 1 ether;
+             saleQuantity -= tokensValue;
+             addr.transfer(saleQuantity);
+             }
+        } else if(stage == ICOStage.remainingSale){
+            saleQuantity = 20000000;
+            while(saleQuantity!=0){
+                tokensValue = (msg.value) * 1e5 / 1 ether;
+                saleQuantity -= tokensValue;
+                addr.transfer(saleQuantity);
+             }
            }
-    }
-
-    function buy() public payable{ 
-        if(stage == ICOStage.remainingSale){
-            addr.transfer(msg.value);
-        }
-    }
- /**   function _forwardFunds() internal{
-        if(stage == ICOStage.PreSale) { 
-         addr.transfer(msg.value);
-       } else if (stage == ICOStage.secondSale) {
-           //super._forwardFunds();
-           }
-    }
+    }   
 }
-       function buyTokens(address _addr, uint256 _saleQuantity) public {
-        _saleQuantity = saleQuantity;
-        _addr.transfer(_saleQuantity);
-    } 
+    /*function buy() public payable{ 
+        
+        if(stage == ICOStage.remainingSale){
+            addr.transfer(tokensValue);
+        }
+    }*/
+
+   /* function forwardFunds() internal {
+      if (stage == ICOStage.PreSale && saleQuantity !=0) {
+          addr.transfer(tokensValue);
+      } else if (stage == ICOStage.secondSale) {
+          //super.forwardFunds();
+      }
+  }
 
 
     function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal{}*/
-
-}
