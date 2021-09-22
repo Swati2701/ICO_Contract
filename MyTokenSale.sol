71160a656a208ee7b8eb507eb28794a2344c3724
Swati Jindal <88382946+Swati2701@users.noreply.github.com>
2021-09-22 14:56:21 +0530

    Add files via upload

--- MyTokenSale.sol
+++ MyTokenSale.sol
@@ -12,17 +12,20 @@ contract MyTokenSale is Crowdsale, Ownable{
     uint256 remainingSaleQuantity;
     uint256 totalSupply;
     uint256 Rate;
+    IERC20 private tokens;
     constructor (uint256 _rate, address payable _wallet, IERC20 _token) Crowdsale(_rate, _wallet, _token) public{
-        
+        preSaleQuantity = 300000000;
+        secondSaleQuantity = 500000000;
+        remainingSaleQuantity = 200000000;
     }
 
     enum ICOStage { PreSale, secondSale, remainingSale}
   
     ICOStage public stage = ICOStage.PreSale;
 
-    function TotalSupply(uint256 _totalSupply) public{
+   /* function TotalSupply(uint256 _totalSupply) public{
           totalSupply = _totalSupply;
-      }
+      }*/
    
     function setICOStage(uint _stage) public onlyOwner{
      
@@ -38,41 +41,48 @@ contract MyTokenSale is Crowdsale, Ownable{
         setCurrentRate(300000);
       } else if (stage == ICOStage.secondSale) {
         setCurrentRate(600000);
-      }
+      } else {
+           
+           // set rate for remainingSale stage
+            setCurrentRate(0);
+        }
     }
 
     function setCurrentRate(uint256 _rate) private {
       Rate = _rate;
     }
 
-    fallback () external payable {
+    function calculateRate() public payable {
     uint256 weiAmount = msg.value;
     uint256 tokenValue = _getTokenAmount(weiAmount);       //msg.value.mul(Rate);
-      
 
-    if ((stage == ICOStage.PreSale)) {
-      preSaleQuantity = 300000000;
-      if(preSaleQuantity == 0){
-          return ;
-      }
-      msg.sender.transfer(tokenValue);
-      preSaleQuantity -= tokenValue;
-      }
 
-      buyTokens(msg.sender);
+    if (stage == ICOStage.PreSale) {
+        require(preSaleQuantity - tokenValue >= 0);
+        //msg.sender.transfer(tokenValue);
+        _forwardFunds();
+        _deliverTokens(addr, tokenValue);
+        preSaleQuantity -= tokenValue;
+        }
 
-    if (stage ==ICOStage.secondSale) {
-      secondSaleQuantity = 500000000;
-      msg.sender.transfer(tokenValue);
-      secondSaleQuantity -= tokenValue;
-      return ;
-    }
-    if (stage ==ICOStage.remainingSale) {
-      remainingSaleQuantity = 200000000;
-      msg.sender.transfer(tokenValue);
-      remainingSaleQuantity -= tokenValue;
-      return ;
-    }
+     if (stage ==ICOStage.secondSale) {
+        require(secondSaleQuantity - tokenValue >= 0);
+        //msg.sender.transfer(tokenValue);
+        _forwardFunds();
+        _deliverTokens(addr, tokenValue);
+        secondSaleQuantity -= tokenValue;
+            return ;
+        }
+        if (stage ==ICOStage.remainingSale) {
+            require(remainingSaleQuantity - tokenValue >= 0);
+            //msg.sender.transfer(tokenValue);
+            _forwardFunds();
+            _deliverTokens(addr, tokenValue);
+            remainingSaleQuantity -= tokenValue;
+            return ;
+        }
+
+        buyTokens(msg.sender);
   }
 
 
@@ -80,12 +90,17 @@ contract MyTokenSale is Crowdsale, Ownable{
         return weiAmount.mul(Rate);
   }
 
-
-  /*function forwardFunds() internal {
+   function forwardFunds() internal {
       if (stage == ICOStage.PreSale) {
           addr.transfer(msg.value);
       } else if (stage == ICOStage.secondSale) {
-          addr.transfer(msg.value)
+          addr.transfer(msg.value);
+      } else {
+          super._forwardFunds();
       }
-  } */
+  } 
+
+  function _deliverTokens(address beneficiary, uint256 tokenAmount) internal override{
+        tokens.safeTransfer(beneficiary, tokenAmount);
+    }
 }
